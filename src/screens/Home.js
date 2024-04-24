b8860c7d9d06803c7162231befe3f69123dd8128
Jovita John <jovitajohn641@gmail.com>
2024-04-24 10:49:44 +0100

    initial Navigation boilerplate

---
+++ src/screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Text,StyleSheet} from 'react-native'
+
+const Home = () => {
+    return <Text> Home screen</Text>
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Home
